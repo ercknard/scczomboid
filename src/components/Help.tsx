4117ef406a1c4877c784ed1f979e629dbe367fec
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2024-01-17 14:08:54 +0800

    1

--- src/components/Help.tsx
+++ src/components/Help.tsx
@@ -5,7 +5,7 @@ export default function Help() {
   return (
     <section className="relative lg:w-full w-[90%] min-h-[100vh] flex justify-center mx-auto items-center mods-1">
       <span className="mods-1" />
-      <div className="text-6xl my-8 text-[white]">
+      <div className="text-6xl my-8 text-[white] z-10">
         {" "}
         <span className="text-[#8f0934]">G</span>
         <span className="text-4xl">ame</span>{" "}
